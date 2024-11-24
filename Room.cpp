0c55b7add5cf494c65a912ea022ac5b8724f95dc
unknown <sgaeteBSD@github.com>
2024-11-24 23:46:14 +0000

    room edits

--- Room.cpp
+++ Room.cpp
@@ -2,22 +2,48 @@
 #include <cstring>
 #include <iostream>
 
-Room::Room(char* description) {
+Room::Room(char* description) { //Constructor
   this->description = new char[strlen(description) + 1];
   strcpy(this->description, description)
 }
 
-Room::~Room() {
+Room::~Room() { //Destructor
   delete[] description;
+
+  for (auto item : items) {
+    delete item;
+  }
 }
 
-char* Room::getShortDescription() {
-  return description;
+void Room::setExit(char* direction, Room* neighbor) { //Set exit to neighbor
+  exits[direction] = neighbor;
 }
 
-char* Room::getLongDescription() {
-  //long description print
+char* Room::getShortDescription() { //Get short desc
+  char* result = new char[strlen(description) + 1];
+  strcpy(result, description);
+  return result;
 }
+
+char* Room::getLongDescription() { //Get long desc
+  //create base
+  int baseLength = strlen("You are in ") + strlen(description) + (strlen(".\n") + 1; //find needed length
+  char* result = new char[baseLength]; //create cstring
+  strcpy(result, "You are in ");
+  strcat(result, description);
+  strcat(result, ".\n")
+
+  //append exit string
+  char* exitString = getExitString();
+  int finalLength = baseLength + strlen(exitString) + 1;
+  char* finalResult = new char[finalLength];
+  strcpy(finalResult, result);
+  strcpy(finalResult, exitString);
+
+  delete[] result;
+  delete[] exitString;
+  return finalResult;								  //a
+								  }
 
 char* Room::getExitString() {
   char* returnString = "Exits:";
--- Room.h
+++ Room.h
@@ -1,4 +1,5 @@
 #ifndef ROOM_H
+#ifndef ROOM_H
 #define ROOM_H
 
 #include "Item.h"
@@ -7,22 +8,25 @@
 
 class Room {
  private:
-  char* description;
-  map<char*, Room*> exits;
-  vector<Item*> items;
+  char* description; //Room description
+  map<char*, Room*> exits; //Exits mapped to their cardinal directions
+  vector<Item*> items; //Items in room
  public:
+  //constructor & destructor
   Room(char* description);
   ~Room();
+
+  //Room management
   void setExit();
   char* getShortDescription();
   char* getLongDescription();
   char* getExitString();
   Room getExit(char* direction);
+
+  //Item management
   Item getItem(int index);
   Item getItemN(char* itemName);
   void removeItem(char* itemName);
   void setItem(Item newItem);
   char* getRoomItems();
 };
-
-#endif
