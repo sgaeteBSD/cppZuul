6625b4474c2d9309abd68e26b6d2b5062de1698e
unknown <sgaeteBSD@github.com>
2024-11-25 00:47:28 +0000

    parser

---
+++ Parser.h
@@ -0,0 +1,18 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include "CommandWords.h"
+#include "Command.h"
+#include <cstring>
+
+class Parser {
+ private:
+    CommandWords commands; //all valid command words
+
+ public:
+    Parser();
+    Command* getCommand();
+    void showCommands(); //prints out valid command words
+};
+
+#endif
